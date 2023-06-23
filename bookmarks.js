d838ba49ebe127ebe63f10efe929f0475e36c6c1
marcellbarsony <barsonymarcell@gmail.com>
2023-06-23 23:51:28 +0200

    Added wrapping

--- bookmarks.js
+++ bookmarks.js
@@ -1,47 +1,107 @@
-// Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Daily",
+    title: "Cyber [CTF]",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
+      { name: "CyberDefenders", url: "https://cyberdefenders.org" },
+      { name: "Hack The Box", url: "https://hackthebox.com" },
+      { name: "Portswigger", url: "https://portswigger.net" },
+      { name: "TryHackMe", url: "https://tryhackme.com" },
+      { name: "Vulnhub", url: "https://vulnhub.com" },
+    ],
+  },
+  {
+    title: "Proton",
+    links: [
+      { name: "Mail", url: "https://proton.me/mail" },
+      { name: "Calendar", url: "https://proton.me/calendar" },
+      { name: "Drive", url: "https://proton.me/drive" },
+      { name: "VPN", url: "https://protonvpn.com" },
+    ],
+  },
+  {
+    title: "Google",
+    links: [
+      { name: "Accounts", url: "https://accounts.google.com" },
       { name: "Drive", url: "https://drive.google.com" },
+      { name: "Flights", url: "https://flights.google.com" },
+      { name: "Gmail", url: "https://mail.google.com" },
+      { name: "Maps", url: "https://maps.google.com" },
+      { name: "MyMaps", url: "https://mymaps.google.com" },
+      { name: "Translate", url: "https://translate.google.com" },
+      { name: "Photos", url: "https://photos.google.com" },
+    ],
+  },
+  {
+    title: "Linux",
+    links: [
+      { name: "Arch", url: "https://archlinux.org" },
+      { name: "Arch AUR", url: "https://aur.archlinux.org" },
+      { name: "Arch Wiki", url: "https://wiki.archlinux.org" },
+      { name: "Distrowatch", url: "https://distrowatch.com" },
+    ],
+  },
+  {
+    title: "Meta",
+    links: [
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Instagram", url: "https://instagram.com" },
+      { name: "Messenger", url: "https://messenger.com" },
+    ],
+  },
+  {
+    title: "Microsoft",
+    links: [
+      { name: "ChatGPT", url: "https://chat.openai.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "OneDrive", url: "https://live.onedrive.com" },
     ],
   },
   {
     title: "Media",
     links: [
+      { name: "Spotify", url: "https://open.spotify.com" },
       { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
+    ],
+  },
+  {
+    title: "News",
+    links: [
+      { name: "/r/worldnews", url: "https://reddit.com/r/worldnews" },
+      { name: "BleepingComputer", url: "https://bleepingcomputer.com" },
+      { name: "HackerNoon", url: "https://hackernoon.com" },
+      { name: "The Daily Swig", url: "https://portswigger.net/daily-swig" },
+      { name: "Telex", url: "https://telex.hu" },
+      { name: "The Hacker News", url: "https://thehackernews.com" },
+      { name: "Qubit", url: "https://qubit.hu" },
     ],
   },
   {
     title: "Reddit",
     links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
+      { name: "Front page", url: "https://reddit.com/" },
+      { name: "/r/archlinux", url: "https://reddit.com/r/archlinux" },
+      { name: "/r/hungary", url: "https://reddit.com/r/hungary" },
+      { name: "/r/neovim", url: "https://reddit.com/r/neovim" },
       { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
+      { name: "/r/qtile", url: "https://reddit.com/r/qtile" },
     ],
   },
   {
     title: "Social",
     links: [
-      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Discord", url: "https://discordapp.com" },
       { name: "Facebook", url: "https://facebook.com" },
+      { name: "Instagram", url: "https://instagram.com" },
+      { name: "Mastodon", url: "https://mastodon.com" },
+      { name: "Nitter", url: "https://nitter.com" },
+      { name: "Twitter", url: "https://twitter.com" },
+    ],
+  },
+  {
+    title: "Tools",
+    links: [
+      { name: "Bitwarden", url: "https://bitwarden.com" },
+      { name: "Privacy Tools", url: "https://privacytools.io" },
     ],
   },
 ];
